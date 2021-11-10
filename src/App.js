2b2c5ea0edfb3fa7c8d395226a0b2fb8d5db94e3
small-j <44703262+small-j@users.noreply.github.com>
2021-11-11 01:05:51 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import "./App.css";
 
 function App() {
+  let a;
   return (
     <div className="App">
       <Button></Button>
+      aaaa
     </div>
   );
 }
 
-function Button(props) {}
+function Button(props) {
+  return <></>;
+}
 
 export default App;
