668d9e0852b9b41b273366eb2f78ea92acb0e767
Hennization <shy6295@ewhain.net>
2021-11-18 03:57:03 +0900

    feat: 문단 개행 및 이미지 import 삽입

--- src/components/episode/WebtoonThumbnail.jsx
+++ src/components/episode/WebtoonThumbnail.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import styled from "styled-components";
+import thumbnail from "../../assets/images/img_jojocomics.png";
 
 const WebtoonThumbnail = () => {
   return (
     <StyledRoot>
-      <StyledThumbnailImg src="../assets/images/img_jojocomics.png" alt="썸네일" />
+      <StyledThumbnailImg src={thumbnail} alt="썸네일" />
       <StyledTitle>조조코믹스</StyledTitle>
       <StyledIntro>
-        <strong>이동건\n</strong>&lt;유미의 세포들&gt; 이동건 작가의 로맨스 시트콤 신작!\n오늘 할 일을 내일로 미루는
-        평범한 직장인 은조.\n 그녀와는 다르게 외모와 매너 모든 것을 철저히 관리하는 직장동료 조완.\n 케미 좋은 두 사람의
-        다양하고 유쾌한 단편들이 여러분들을 기다리고 있습니다.\n매주 수토! &lt;조조코믹스&gt;에서 단편만화 같은 일상을
-        즐겨보세요!
+        <strong>이동건</strong>
+        <p>&lt;유미의 세포들&gt; 이동건 작가의 로맨스 시트콤 신작!</p>
+        <p>오늘 할 일을 내일로 미루는 평범한 직장인 은조.</p>
+        <p>그녀와는 다르게 외모와 매너 모든 것을 철저히 관리하는 직장동료 조완.</p>
+        <p>케미 좋은 두 사람의 다양하고 유쾌한 단편들이 여러분들을 기다리고 있습니다.</p>
+        <p>매주 수토! &lt;조조코믹스&gt;에서 단편만화 같은 일상을 즐겨보세요!</p>
       </StyledIntro>
       <StyledBtn>
         <button>관심웹툰</button>
--- src/pages/Episode.jsx
+++ src/pages/Episode.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import WebtoonThumbnail from "../components/episode/WebtoonThumbnail";
 
 const Episode = () => {
-  return <div>episode</div>;
+  return (
+    <div>
+      <WebtoonThumbnail />
+    </div>
+  );
 };
 
 export default Episode;
