2269e21ca02429f7f62015c259e2a342e748c016
Hennization <shy6295@ewhain.net>
2021-11-19 16:39:24 +0900

    feat: 오늘의 웹툰 이미지 슬레이터 틀 스타이링

--- src/components/home/ImgSlader.jsx
+++ src/components/home/ImgSlader.jsx
@@ -1,18 +1,36 @@
 import React from "react";
+import styled from "styled-components";
+import today1 from "../../assets/images/img_today1.png";
+import today2 from "../../assets/images/img_today2.png";
+import today3 from "../../assets/images/img_today3.png";
 
 const ImgSlader = () => {
   return (
-    <div>
+    <StyledRoot>
       <div>11월 10일 수요일</div>
-      <img />
+      <img src={today1} />
+      <img src={today2} />
+      <img src={today3} />
       <button>왼쪽 화살표</button>
       <button>오른쪽 화살표</button>
       {/*이미지 및 동그란 버튼*/}
       <button></button>
       <button></button>
       <button></button>
-    </div>
+    </StyledRoot>
   );
 };
 
 export default ImgSlader;
+
+const StyledRoot = styled.div`
+  border: 0.2rem solid #e5e5e5;
+  width: 69.169rem;
+  height: 33.2rem;
+
+  & > img {
+    width: 30.6rem;
+    height: 16.8rem;
+    border-radius: 0.6rem;
+  }
+`;
--- src/components/home/TodayWebtoon.jsx
+++ src/components/home/TodayWebtoon.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import styled from "styled-components";
 import ImgSlader from "./ImgSlader";
-import newWebtoon from "../../assets/images/img_new";
+import newWebtoon from "../../assets/images/img_new.png";
+import uploadIcon from "../../assets/icons/ic_upload.png";
+import viewMoreIcon from "../../assets/icons/ic_viewmore.png";
 
 const TodayWebtoon = () => {
   return (
@@ -25,13 +27,21 @@ const TodayWebtoon = () => {
             <p>베스트 도전만화</p>
           </div>
           <img src={newWebtoon} alt="새로운 베스트 도전만화" />
-          <p>이게좋아</p>
-          <p>lobyeol</p>
+          <div>
+            <p>이게좋아</p>
+            <p>lobyeol</p>
+          </div>
         </StyledNewComic>
         <StyledBtn>
-          <button>웹툰 올리기</button>
-          <button>고객센터</button>
-          <button>공지사항</button>
+          <button>
+            웹툰 올리기 <img src={uploadIcon} />
+          </button>
+          <button>
+            고객센터 <img src={viewMoreIcon} />
+          </button>
+          <button>
+            공지사항 <img src={viewMoreIcon} />
+          </button>
         </StyledBtn>
       </StyledRight>
     </StyledRoot>
@@ -42,25 +52,98 @@ export default TodayWebtoon;
 
 const StyledRoot = styled.div``;
 
-const StyledTodayWebtoon = styled.div``;
+const StyledTodayWebtoon = styled.div`
+  & > div {
+    font-size: 2.8rem;
+    font-weight: bold;
+    & > strong {
+      color: #60d171;
+    }
+  }
+`;
 
-const StyledNav = styled.div``;
+const StyledNav = styled.div`
+  font-weight: bold;
+  & > button {
+    border-radius: 1.35rem;
+    width: 6.111rem;
+    height: 2.037rem;
+    border: none;
+    background-color: #e8e8e8;
+    margin-bottom: 1.663rem;
+  }
 
-const StyledRight = styled.div``;
+  & > button:nth-child(2) {
+    color: white;
+    background-color: #60d171;
+  }
+
+  & > button:nth-child(4) {
+    margin-bottom: 0;
+  }
+`;
+
+const StyledRight = styled.div`
+  margin-left: 2.116rem;
+`;
 
 const StyledNewComic = styled.div`
   border-radius: 0.5rem;
-  border-style: 0.1rem solid #e5e5e5;
+  border: 0.1rem solid #e5e5e5;
   background-color: #f9f9fc;
-  & > div {
+  width: 18.3rem;
+  height: 20.371rem;
+  margin-left: 2.116rem;
+  margin-bottom: 1.613rem;
+  padding-left: 1.3rem;
+  padding-top: 1.3rem;
+  padding-bottom: 1.471rem;
+
+  & > div:nth-child(1) {
     font-weight: bold;
     font-size: 1.5rem;
+    margin-bottom: 0.985rem;
     & > strong {
       color: #60d171;
     }
     & > p {
     }
   }
+
+  & > img {
+    width: 15.828rem;
+    height: 8.6rem;
+  }
+
+  & > div:nth-child(3) {
+    font-weight: bold;
+    & > p:nth-child(1) {
+      font-size: 1.4rem;
+      margin-top: 1.115rem;
+      margin-bottom: 0.6rem;
+    }
+    & > p:nth-child(2) {
+      font-size: 1rem;
+      color: #737373;
+    }
+  }
 `;
 
-const StyledBtn = styled.div``;
+const StyledBtn = styled.div`
+  & > button {
+    border: 0.1rem solid #e5e5e5;
+    background-color: #f9f9fc;
+    margin-right: 1.321rem;
+    width: 18.273rem;
+    height: 3.2rem;
+    border-radius: 0.6rem;
+    font-size: 1.4rem;
+  }
+
+  & > button:nth-child(1) {
+    color: white;
+    background-color: #60d171;
+    border: none;
+    margin-right: 0.323rem;
+  }
+`;
