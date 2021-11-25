3ce555111970a7cb95c8001f18cd1616b481a059
Hennization <shy6295@ewhain.net>
2021-11-25 14:16:14 +0900

    feat: commentInfo 받아오기

--- src/components/episode/CommentsList.jsx
+++ src/components/episode/CommentsList.jsx
@@ -3,8 +3,44 @@ import styled from "styled-components";
 import BestComment from "./BestComment";
 import cleanBot from "../../assets/images/img_cleanbot.png";
 import check from "../../assets/icons/ic_check.png";
+import bestIcon from "../../assets/icons/ic_bestcomment.png";
 
 const CommentsList = () => {
+  const commentsInfo = [
+    {
+      img: { bestIcon }, //객체는 :
+      name: "유진",
+      id: "haneugene",
+      date: "2021-10-18",
+      content:
+        "하루 종일 너란 바닷속을 항해하는 나는 아쿠아맨 헤엄 헤엄 헤엄\nI&apos;m rolling in the deep inside of you\n너의 어장은 너무 캄캄해",
+    },
+    {
+      img: { bestIcon }, //객체는 :
+      name: "유진",
+      id: "haneugene",
+      date: "2021-10-18",
+      content:
+        "하루 종일 너란 바닷속을 항해하는 나는 아쿠아맨 헤엄 헤엄 헤엄\nI&apos;m rolling in the deep inside of you\n너의 어장은 너무 캄캄해",
+    },
+    {
+      img: { bestIcon }, //객체는 :
+      name: "유진",
+      id: "haneugene",
+      date: "2021-10-18",
+      content:
+        "하루 종일 너란 바닷속을 항해하는 나는 아쿠아맨 헤엄 헤엄 헤엄\nI&apos;m rolling in the deep inside of you\n너의 어장은 너무 캄캄해",
+    },
+    {
+      img: { bestIcon }, //객체는 :
+      name: "유진",
+      id: "haneugene",
+      date: "2021-10-18",
+      content:
+        "하루 종일 너란 바닷속을 항해하는 나는 아쿠아맨 헤엄 헤엄 헤엄\nI&apos;m rolling in the deep inside of you\n너의 어장은 너무 캄캄해",
+    },
+  ];
+
   return (
     <StyledRoot>
       <StyledBtnWrapper>
@@ -23,14 +59,20 @@ const CommentsList = () => {
         </div>
         <div>설정</div>
       </StyledBox>
-      <BestComment />
+      {commentsInfo.map((commentInfo) => (
+        <BestComment key={commentInfo} />
+      ))}
     </StyledRoot>
   );
 };
 
 export default CommentsList;
 
-const StyledRoot = styled.div``;
+const StyledRoot = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-right: 19.8rem;
+`;
 
 const StyledBtnWrapper = styled.div`
   font-size: 1.25rem;
@@ -39,16 +81,17 @@ const StyledBtnWrapper = styled.div`
     border: none;
     background-color: white;
     margin-top: 3.8rem;
+    font-weight: bold;
   }
 
   & > button:nth-child(1) {
     color: #60d171;
     margin-right: 3.2rem;
-    img {
+    & > img {
       margin-right: 0.6rem;
+      vertical-align: middle;
     }
   }
-
   & > button:nth-child(2) {
     color: #d2d2d2;
   }
@@ -61,22 +104,29 @@ const StyledBox = styled.div`
   margin-top: 1.7rem;
   margin-bottom: 2.4rem;
   color: #d2d2d2;
-  width: 66.4rem;
   height: 4rem;
   display: flex;
   justify-content: space-between;
 
   & > div:nth-child(1) {
-    line-height: 2rem;
-    span {
-      strong {
+    display: flex;
+    align-items: center;
+
+    & > img {
+      margin-left: 1rem;
+      margin-right: 0.5rem;
+    }
+
+    & > span {
+      & > strong {
         color: #60d171;
       }
     }
   }
 
   & > div:nth-child(2) {
-    padding-top: 1.4rem;
+    display: flex;
+    align-items: center;
     margin-right: 1.8rem;
   }
 `;
