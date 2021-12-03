81a27bfca86d8bb2b6d8f0416057235759adf648
Sohee KWON <soheekwon@naver.com>
2021-12-03 18:41:43 +0900

    feat: 의견쓰기 구현

--- src/components/episode/CommentWriteMain.jsx
+++ src/components/episode/CommentWriteMain.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import WriterComment from "./commentwrite/WriterComment";
 import StarScore from "./commentwrite/StarScore";
+import Comment from "./commentwrite/Comment";
+import { postComment } from "../../libs/api";
 
-const CommentWriteMain = ({ writer = "이동건" }) => {
+const CommentWriteMain = ({ writer = "이동건", webtoonId = 1, email = "abc@mail.com" }) => {
+  const handleSubmit = async (e, input) => {
+    e.preventDefault();
+    const data = await postComment({ webtoonId, email, input });
+    console.log(data);
+  };
+
   return (
     <>
       <WriterComment writer={writer} />
       <StarScore />
+      <Comment handleSubmit={handleSubmit} />
     </>
   );
 };
