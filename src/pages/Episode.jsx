b0a23c2c416ef93dd8618ab8a48dd38b4584b1d5
Hennization <shy6295@ewhain.net>
2021-12-03 18:45:44 +0900

    feat: 댓글 목록 서버 연결

--- src/pages/Episode.jsx
+++ src/pages/Episode.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+// import { useLocation } from "react-router-dom";
 import WebtoonThumbnail from "../components/episode/WebtoonThumbnail";
 import CommentsListHeader from "../components/episode/CommentsListHeader";
 import CommentsListMain from "../components/episode/CommentsListMain";
@@ -7,12 +8,15 @@ import CommentsListFooter from "../components/episode/CommentsListFooter";
 
 const Episode = () => {
   const [view, setView] = useState("");
+  // const location = useLocation();
+  // const id = location.state.id;
+  const id = 1;
 
   return (
     <div>
       <WebtoonThumbnail />
       <CommentsListHeader setView={setView} />
-      <CommentsListMain view={view} />
+      <CommentsListMain view={view} id={id} />
       <CommentsListFooter />
     </div>
   );
