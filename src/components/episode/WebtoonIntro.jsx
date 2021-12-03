b690a8b430aab402371ee01ee011950ba8a086f7
Hennization <shy6295@ewhain.net>
2021-12-03 15:20:52 +0900

    feat: 반응형 구현

--- src/components/episode/WebtoonIntro.jsx
+++ src/components/episode/WebtoonIntro.jsx
@@ -9,18 +9,25 @@ const WebtoonIntro = () => {
       <StyledIntro>
         <h2>조조코믹스</h2>
         <h5>이동건</h5>
-        <Screen desktop tablet mobile>
-          <p>&lt;유미의 세포들&gt; 이동건 작가의 로맨스 시트콤 신작!</p>
-          <p>오늘 할 일을 내일로 미루는 평범한 직장인 은조.</p>
-          <p>그녀와는 다르게 외모와 매너 모든 것을 철저히 관리하는 직장동료 조완.</p>
-          <p>케미 좋은 두 사람의 다양하고 유쾌한 단편들이 여러분들을 기다리고 있습니다.</p>
-          <p>매주 수토! &lt;조조코믹스&gt;에서 단편만화 같은 일상을 즐겨보세요!</p>
+        <Screen desktop tablet>
+          <p>
+            &lt;유미의 세포들&gt; 이동건 작가의 로맨스 시트콤 신작!
+            <br />
+            오늘 할 일을 내일로 미루는 평범한 직장인 은조.
+            <br />
+            그녀와는 다르게 외모와 매너 모든 것을 철저히 관리하는 직장동료 조완.
+            <br />
+            케미 좋은 두 사람의 다양하고 유쾌한 단편들이 여러분들을 기다리고 있습니다.
+            <br />
+            매주 수토! &lt;조조코믹스&gt;에서 단편만화 같은 일상을 즐겨보세요!
+          </p>
         </Screen>
-        {/* <Screen mobile>
-          <p>&lt;유미의 세포들&gt; 이동건 작가의 로맨스 시트콤 신작!</p>
-          <p>오늘 할 일을 내일로 미루는 평범한 직장인 은조.</p>
-          <p>그녀와는 다르게 외모와 매너 모든 것을 철저히 관리하는...</p>
-        </Screen> */}
+        <Screen mobile>
+          <p>
+            &lt;유미의 세포들&gt; 이동건 작가의 로맨스 시트콤 신작! 오늘 할 일을 내일로 미루는 평범한 직장인 은조.
+            그녀와는 다르게 외모와 매너 모든 것을 철저히 관리하는...
+          </p>
+        </Screen>
       </StyledIntro>
     </StyledRoot>
   );
@@ -56,10 +63,9 @@ const StyledIntro = styled.section`
 
   p {
     line-height: 1.9rem;
+    white-space: nowrap;
     ${applyMediaQuery("mobile")} {
-      /* width: 19.9rem;
-      height: 9.6rem;
-      text-overflow: ellipsis; */
+      white-space: normal;
     }
   }
 `;
