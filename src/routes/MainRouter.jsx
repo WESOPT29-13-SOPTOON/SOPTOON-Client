cc85d32fcbf7fe88070cc0808c9db5d7d1f6a029
Sohee KWON <soheekwon@naver.com>
2021-12-03 15:35:57 +0900

    feat: 메인 헤더 구현

--- src/routes/MainRouter.jsx
+++ src/routes/MainRouter.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Episode from "../pages/Episode";
 import Home from "../pages/Home";
+import MainHeader from "../components/common/MainHeader";
 
 const MainRouter = () => {
   return (
     <BrowserRouter>
+      <MainHeader />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/episode" element={<Episode />} />
