d5c3c201a80ba92c9118d1de2420d4a0fe3168f6
Sohee KWON <soheekwon@naver.com>
2021-12-04 01:22:54 +0900

    feat: 의견 쓰기 반응형 추가

--- src/components/episode/commentwrite/StarScore.jsx
+++ src/components/episode/commentwrite/StarScore.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { star1, star2, star3, star4, star5, star6, star7, star8, star9, star10 } from "../../../assets/icons/star";
 import styled from "styled-components";
+import { applyMediaQuery } from "../../../styles/mediaQuery";
 
 const StarScore = () => {
   const [score, setScore] = useState(star10);
@@ -19,9 +20,11 @@ const StarScore = () => {
 
   return (
     <StyledRoot>
-      <p>별점 주기</p>
-      <img className="score" src={score} onClick={handleOpenClick} alt="star" />
-      <button>확인</button>
+      <StyledTop>
+        <p>별점 주기</p>
+        <img className="score" src={score} onClick={handleOpenClick} alt="star" />
+        <button>확인</button>
+      </StyledTop>
       {showOption && (
         <ul>
           {starList.map((star, index) => (
@@ -38,10 +41,35 @@ const StarScore = () => {
 const StyledRoot = styled.div`
   width: var(--screen-width);
   height: 90px;
-  display: flex;
-  align-items: center;
   padding-left: 20px;
   margin: 0 auto;
+  ul {
+    background-color: white;
+    padding: 3px 6px;
+    border-radius: 4px;
+    position: relative;
+    left: 85px;
+    top: -10px;
+    &:hover {
+      cursor: pointer;
+    }
+  }
+  li {
+    padding: 3px 0;
+  }
+
+  ${applyMediaQuery("mobile")} {
+    height: 75px;
+    padding-left: 0;
+    p {
+      font-size: 15px;
+    }
+  }
+`;
+
+const StyledTop = styled.div`
+  display: flex;
+  align-items: center;
   p {
     font-size: 20px;
     font-weight: bold;
@@ -60,20 +88,6 @@ const StyledRoot = styled.div`
     width: 34px;
     height: 20px;
     border-radius: 10px;
-  }
-  ul {
-    background-color: white;
-    padding: 3px 6px;
-    border-radius: 4px;
-    position: relative;
-    left: -165px;
-    top: 150px;
-    &:hover {
-      cursor: pointer;
-    }
-  }
-  li {
-    padding: 3px 0;
   }
 `;
 
